53a54d5953af2de3c78226d3e9d750d4f17c0928
avendcar <105431900+avendcar@users.noreply.github.com>
2025-02-24 16:24:51 -0600

    Update profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_3/main.dart';
 
 void main() {
   runApp(ProfilePage());
