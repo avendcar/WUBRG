a5bf69730fa49ad521571f2666e31e438c907555
avendcar <avendcar@outlook.com>
2025-03-04 15:09:03 -0600

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,24 @@ void main() {
 }
 
 String? _dropdownValue;
+String displayedDropdownText = "Search filters";
 
-void dropdownCallBack(String? selectedValue) {
-  if (selectedValue is String) {
-    _dropdownValue = selectedValue;
-  }
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class _HomePageState extends State<HomePage> {
+  void dropdownCallBack(String? selectedValue) {
+    if (selectedValue is String) {
+      setState(() {
+        _dropdownValue = selectedValue;
+        displayedDropdownText = selectedValue;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,44 +32,48 @@ class HomePage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: Builder(builder: (context) {
-            return IconButton(
-              color: Colors.white,
-              iconSize: 40,
-              onPressed: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-              },
-              icon: const Icon(Icons.home),
-            );
-          }),
+          leading: Builder(
+            builder: (context) {
+              return IconButton(
+                color: Colors.white,
+                iconSize: 40,
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+                icon: const Icon(Icons.home),
+              );
+            },
+          ),
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))),
           actions: [
             SearchBar(
+              backgroundColor: WidgetStatePropertyAll(Colors.grey),
               constraints: BoxConstraints(maxWidth: 450, minHeight: 30),
               leading: Icon(Icons.search),
               hintText: "Search...",
             ),
             Container(
+              //Search filters dropdown box
               width: 130,
               height: 30,
               decoration: BoxDecoration(
-                color: Colors.grey,
-                shape: BoxShape.rectangle,
-                border: Border.all(width: 3, color: Colors.white),
-                borderRadius: BorderRadius.circular(10)
-              ),
+                  color: Colors.grey,
+                  shape: BoxShape.rectangle,
+                  border: Border.all(width: 3, color: Colors.white),
+                  borderRadius: BorderRadius.circular(10)),
               alignment: Alignment.center,
               child: SizedBox(
                 width: 125,
-                child: new DropdownButtonHideUnderline(
+                child: DropdownButtonHideUnderline(
                   child: DropdownButton(
+                    onTap: () {},
                     hint: Text(
-                      "Search filters",
+                      displayedDropdownText,
                       selectionColor: Colors.white,
                       textAlign: TextAlign.justify,
                     ),
