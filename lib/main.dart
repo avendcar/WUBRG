9b0c8eb3eb554e9676a7f3e5c72f5c19e60ea723
avendcar <avendcar@outlook.com>
2025-03-04 22:01:02 -0600

    new file:   .vscode/settings.json 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,122 +34,151 @@ class _HomePageState extends State<HomePage> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: Builder(
-            builder: (context) {
-              return IconButton(
-                color: Colors.white,
-                iconSize: 40,
-                onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(10),
+            ),
+          ),
+          leading: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Builder(
+                //Consiste
+                builder: (context) {
+                  return IconButton(
+                    padding: EdgeInsets.only(left: 5, right: 20),
+                    color: Colors.white,
+                    iconSize: 50,
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    },
+                    icon: const Icon(Icons.home),
                   );
                 },
-                icon: const Icon(Icons.home),
-              );
-            },
-          ),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))),
-          actions: [
-            SearchBar(
-              backgroundColor: WidgetStatePropertyAll(Colors.grey),
-              constraints: BoxConstraints(maxWidth: 450, minHeight: 30),
-              leading: Icon(Icons.search),
-              hintText: "Search...",
-            ),
-            Container(
-              //Search filters dropdown box
-              width: 130,
-              height: 30,
-              decoration: BoxDecoration(
-                  color: Colors.grey,
-                  shape: BoxShape.rectangle,
-                  border: Border.all(width: 3, color: Colors.white),
-                  borderRadius: BorderRadius.circular(10)),
-              alignment: Alignment.center,
-              child: SizedBox(
-                width: 125,
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton(
-                    onTap: () {},
-                    hint: Text(
-                      displayedDropdownText,
-                      selectionColor: Colors.white,
-                      textAlign: TextAlign.justify,
+              ),
+              Text(
+                'W U B R G',
+                style: TextStyle(
+                    fontFamily: 'Belwe', color: Colors.white, fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: SearchBar(
+                  //Acts as the main search bar for the site
+                  backgroundColor: WidgetStatePropertyAll(Colors.grey),
+                  constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * .4,
+                      minHeight: 30),
+                  leading: Icon(Icons.search),
+                  hintText: "Search...",
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Container(
+                  //Sets desired search filter
+                  width: 130,
+                  height: 30,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.rectangle,
+                      border: Border.all(width: 3, color: Colors.white),
+                      borderRadius: BorderRadius.circular(10)),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 125,
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton(
+                        dropdownColor: Colors.grey,
+                        onTap: () {},
+                        hint: Text(
+                          displayedDropdownText,
+                          selectionColor: Colors.white,
+                          textAlign: TextAlign.justify,
+                        ),
+                        alignment: Alignment.centerLeft,
+                        items: const [
+                          DropdownMenuItem(
+                            value: "All",
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 50),
+                              child: Text(
+                                "All",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontFamily: "Belwe"),
+                              ),
+                            ),
+                          ),
+                          DropdownMenuItem(
+                            value: "Events",
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 40),
+                              child: Text(
+                                "Events",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontFamily: "Belwe"),
+                              ),
+                            ),
+                          ),
+                          DropdownMenuItem(
+                            value: "Players",
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 40),
+                              child: Text(
+                                "Players",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontFamily: "Belwe"),
+                              ),
+                            ),
+                          ),
+                        ],
+                        value: _dropdownValue,
+                        onChanged: dropdownChangeText,
+                      ),
                     ),
-                    alignment: Alignment.centerLeft,
-                    items: const [
-                      DropdownMenuItem(
-                        value: "All",
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 50),
-                          child: Text(
-                            "All",
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      DropdownMenuItem(
-                        value: "Events",
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 40),
-                          child: Text(
-                            "Events",
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      DropdownMenuItem(
-                        value: "Players",
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 40),
-                          child: Text(
-                            "Players",
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ],
-                    value: _dropdownValue,
-                    onChanged: dropdownChangeText,
                   ),
                 ),
               ),
+            ],
+          ),
+          leadingWidth: MediaQuery.of(context).size.width,
+          actions: [
+            Builder(
+              builder: (context) {
+                return TextButton(
+                  //Transitions to profile page
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ProfilePage()),
+                    );
+                  },
+                  child: Icon(Icons.person, color: Colors.white),
+                );
+              },
             ),
-            Builder(builder: (context) {
-              return TextButton(
-                //Transitions to profile page
-                onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ProfilePage()),
-                  );
-                },
-                child: Icon(Icons.person, color: Colors.white),
-              );
-            }),
           ],
           backgroundColor: Colors.black,
-          title: Text(
-            textAlign: TextAlign.center,
-            'W U B R G',
-            style: TextStyle(fontFamily: 'Belwe', color: Colors.white),
-          ),
         ),
         body: Container(
-          //Background Image
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("images/mtg-background.png"),
-                fit: BoxFit.cover),
-          ),
-          child: null
-          //Main Content
-        ),
+            //Background Image
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/mtg-background.png"),
+                  fit: BoxFit.cover),
+            ),
+            child: null
+            //Main Content
+            ),
       ),
     );
   }
