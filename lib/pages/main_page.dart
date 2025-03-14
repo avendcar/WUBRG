627a230b6f46597f8c5c20791bf3eb1e626eb2c3
avendcar <105431900+avendcar@users.noreply.github.com>
2025-03-14 15:42:32 -0500

    Wubrg experimental (#3)
    * deleted:    lib/login.dart
    	modified:   lib/main.dart
    	new file:   lib/pages/home_page.dart
    	new file:   lib/pages/main_page.dart
    	new file:   lib/pages/profile_page.dart
    	deleted:    lib/profile_page.dart
    	new file:   lib/widgets/app_drawer.dart
    	new file:   python_server/server.py
    
    * Inserted the WUBRG background image onto the signed in main page
    	modified:   lib/pages/home_page.dart
    	modified:   lib/pages/main_page.dart
    	modified:   lib/pages/profile_page.dart
    
    * modified:   lib/pages/main_page.dart
    	modified:   lib/pages/profile_page.dart
    
    * modified:   lib/pages/home_page.dart
    	modified:   lib/pages/main_page.dart
    	modified:   lib/pages/profile_page.dart
    	new file:   lib/widgets/app_bar.dart
    	modified:   lib/widgets/app_drawer.dart

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,123 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/pages/profile_page.dart';
+import 'package:flutter_application_3/widgets/app_bar.dart';
+import 'package:flutter_application_3/widgets/app_drawer.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // Add a Drawer to the Scaffold
-      drawer: Drawer(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: 40), // Top spacing
-              // Box for Events
-              Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Text(
-                  'Events',
-                  style: TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              SizedBox(height: 16),
-              // Box for Find
-              Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Text(
-                  'Find',
-                  style: TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              SizedBox(height: 16),
-              // Box for User's Deck
-              Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Text(
-                  "Message",
-                  style: TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Row(
-          children: [
-            // Wrap the logo in a GestureDetector to open the drawer.
-            Builder(
-              builder: (context) {
-                return GestureDetector(
-                  onTap: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  child: Text(
-                    "WUBRG",
-                    style: TextStyle(
-                      fontFamily: "Belwe",
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                );
-              },
-            ),
-            Spacer(),
-            // Search bar widget.
-            SizedBox(
-              width: 200,
-              height: 35,
-              child: TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: "Search...",
-                  hintStyle: TextStyle(color: Colors.white70),
-                  filled: true,
-                  fillColor: Colors.grey[800],
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-            ),
-            Spacer(),
-            // Profile icon.
-            IconButton(
-              icon: Icon(Icons.person),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ProfilePage()),
-                );
-              },
-              color: Colors.white,
-            ),
-          ],
-        ),
-      ),
+      endDrawer: AppDrawer(),
+      appBar: PersistentAppBar(),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
