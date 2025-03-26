ae03e3e5a45b06381be62e7f9b7670852c962d4a
avendcar <avendcar@outlook.com>
2025-03-26 18:21:17 -0500

    modified:   .dart_tool/package_config.json 	modified:   lib/objects/tables.dart

--- lib/objects/tables.dart
+++ lib/objects/tables.dart
@@ -1,15 +1,17 @@
 import 'package:flutter_application_3/objects/user.dart';
 
 class Table {
-  Table(this.tableSize);
+  Table(this.tableSize, this.players);
+  //Each table will have a list of players and a max size
   int tableSize;
+  List<User> players;
 }
 
 //Contains a list of all of an event's tables
 List<Table> tables = [];
 
-void addTable(int numOfPlayers) {
-  Table table = Table(numOfPlayers);
+void addTable(int numOfPlayers, List<User> players) {
+  Table table = Table(numOfPlayers, players);
   tables.add(table);
 }
 
